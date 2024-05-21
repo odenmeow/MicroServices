2e636fed05fb4a1d99682a4cdd86b0e4ec0ab90e
Oni <OniLin@universalec.com>
2024-05-21 18:11:08 +0800

    [MicroServices] 暫存:D

--- swiggy-gateway/src/main/java/com/example/swiggygateway/filter/AuthenticationFilter.java
+++ swiggy-gateway/src/main/java/com/example/swiggygateway/filter/AuthenticationFilter.java
@@ -2,10 +2,13 @@ package com.example.swiggygateway.filter;
 
 import com.example.swiggygateway.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
     @Autowired
     private RouteValidator validator;
---
+++ swiggy-service-registry/src/main/java/com/example/swiggyserviceregistry/SwiggyServiceRegistryApplication.java
@@ -0,0 +1,14 @@
+package com.example.swiggyserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@
+public class SwiggyServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiggyServiceRegistryApplication.class, args);
+	}
+
+}
