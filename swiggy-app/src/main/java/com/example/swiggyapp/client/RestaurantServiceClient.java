c3af3a12c074b1e194b633ceb57238590ace4c7b
Oni <OniLin@universalec.com>
2024-05-28 12:09:08 +0800

    [service call service] 目前考慮 RestTemplate送出的時候 使用固定JWT (特殊角色) ，就不會被 JwtPreAuthFilter卡住

--- swiggy-app/src/main/java/com/example/swiggyapp/client/RestaurantServiceClient.java
+++ swiggy-app/src/main/java/com/example/swiggyapp/client/RestaurantServiceClient.java
@@ -15,4 +15,16 @@ public class RestaurantServiceClient {
     public OrderResponseDTO fetchOrderStatus(String orderId) {
         return template.getForObject("http://RESTAURANT-SERVICE/restaurant/orders/status/" + orderId, OrderResponseDTO.class);
     }
+
+//    @GetMapping("/callServiceB")
+//    public ResponseEntity<String> callServiceB(@RequestHeader("Authorization") String token) {
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setBearerAuth(token.substring(7)); // 確保去掉 'Bearer ' 前綴
+//        HttpEntity<String> entity = new HttpEntity<>(headers);
+//
+//        ResponseEntity<String> response = restTemplate.exchange(
+//                "http://serviceB/api/resource", HttpMethod.GET, entity, String.class);
+//
+//        return ResponseEntity.ok(response.getBody());
+//    }
 }
